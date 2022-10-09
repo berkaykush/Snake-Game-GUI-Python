995f17b6bf2bdee92c54b598e0342b08fa100c8b
Berkay Kush <kushberkay0511@gmail.com>
2022-10-09 18:38:10 +0200

    Add the button class that is used for creating the buttons for the menus.

---
+++ code/buttons.py
@@ -0,0 +1,66 @@
+import pygame
+
+from colors import COLORS
+from points import CELL_HEIGHT, CELL_WIDTH
+
+
+class Button:
+    def __init__(self, font_type, name, position, elevation):
+        font_size = min(int(CELL_WIDTH * 1.5), int(CELL_HEIGHT * 1.5))
+        text_font = pygame.font.Font(font_type, font_size)
+
+        width = CELL_WIDTH * 10
+        height = CELL_HEIGHT * 3
+
+        self.__name = name
+        self.__position = position
+        self.__elevation = elevation
+
+        self.__bottom_rect = pygame.Rect(
+            position.get_x * CELL_WIDTH, position.get_y * CELL_HEIGHT, width, self.__elevation)
+        self.__bottom_rect_color = COLORS['DARK BLUE']
+
+        self.__top_rect = pygame.Rect(
+            position.get_x * CELL_WIDTH, self.__position.get_y * CELL_HEIGHT, width, height)
+        self.__top_rect_color = COLORS['BLUE']
+
+        self.__text_surface = text_font.render(name, True, COLORS['WHITE'])
+        self.__text_rect = self.__text_surface.get_rect(
+            center=self.__top_rect.center)
+
+    @property
+    def get_top_rect(self):
+        return self.__top_rect
+
+    @property
+    def get_name(self):
+        return self.__name
+
+    @property
+    def get_elevation(self):
+        return self.__elevation
+
+    def set_elevation(self, new_value):
+        self.__elevation = new_value
+
+    def change_color(self, top_rect_color, bottom_rect_color):
+        self.__top_rect_color = top_rect_color
+        self.__bottom_rect_color = bottom_rect_color
+
+    def draw(self, screen):
+        self.__top_rect.y = (self.__position.get_y *
+                             CELL_HEIGHT) - self.__elevation
+        self.__text_rect.center = self.__top_rect.center
+
+        self.__bottom_rect.midtop = self.__top_rect.midtop
+        self.__bottom_rect.height = self.__top_rect.height + self.__elevation
+
+        pygame.draw.rect(screen, self.__bottom_rect_color,
+                         self.__bottom_rect, border_radius=100)
+        pygame.draw.rect(screen, self.__top_rect_color,
+                         self.__top_rect, border_radius=100)
+
+        screen.blit(self.__text_surface, self.__text_rect)
+
+    def __eq__(self, other):
+        return (isinstance(other, Button)) and (self.__name == other.get_name)
